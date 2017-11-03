ce50ceec34fec5ed45d5b0584307df40352c621b
Jonathan Duffy <jcduffy5@gmail.com>
2017-11-03 19:58:15 -0400

    Menu Navigation scripts.

---
+++ Api/UnityAPI/Tableau/Assets/API/Includes/Tableau/MenuScripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneOnClick : MonoBehaviour {
+    //to use this, the index refers to the scene index that you will set in the projects build settings.
+	public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene (sceneIndex);
+    }
+
+}
