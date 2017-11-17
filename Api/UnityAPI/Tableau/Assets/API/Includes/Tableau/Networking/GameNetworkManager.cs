d696d4696bbcb90078fe885ba6fe76ade9560088
James Hoak <jah272@case.edu>
2017-11-17 17:16:25 -0600

    Add classes to namespace

--- Api/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/GameNetworkManager.cs
+++ Api/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/GameNetworkManager.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 //keeps track of the state of the game (who's turn it is, cards left in the deck, state of the board, etc.)
-public class GameNetworkManager : MonoBehaviour {
 
-	
+namespace Tableau.Base.Net {
+	public class GameNetworkManager : MonoBehaviour {
+
+		
+	}
 }
