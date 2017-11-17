68e446a9c5763cc70f464373775fd76fc7c2883a
James Hoak <jah272@case.edu>
2017-11-17 17:17:07 -0600

    Add CardPlayer to namespace

--- API/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/CardGamePlayer.cs
+++ API/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/CardGamePlayer.cs
@@ -3,85 +3,87 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 using Tableau.Base;
 
-public class Player : NetworkBehaviour {
+namespace Tableau.Base.Net {
+    public class Player : NetworkBehaviour {
 
-    public int maxHandSize = 5;
+        public int maxHandSize = 5;
 
-    //Use [SyncVar] to indicate which variables should be synchronized.
-    [SyncVar]
-    public Card[] handCards;
+        //Use [SyncVar] to indicate which variables should be synchronized.
+        [SyncVar]
+        public Card[] handCards;
 
-    [SyncVar]
-    public int playerId;
+        [SyncVar]
+        public int playerId;
 
 
-    public override void OnStartClient() {
-        
-    }
-
-    public override void OnNetworkDestroy() {
-    }
-
-    public override void OnStartServer() {
-
-    }
-
-    public override void OnStartLocalPlayer() {
-
-    }
-
-    [Server]
-    public void ServerAddCard(Card newCard) {
-        if (handCards.Length + 1 < maxHandSize) {
-            handCards.Add(newCard);
-            RpcAddCard(newCard);
+        public override void OnStartClient() {
+            
         }
-    }
-    //RPC = Remote Procedure Call. The way to perform an action across a network.
-    //[ClientRpc] is called on the server, but runs on the client.
-    [ClientRpc]
-    void RpcAddCard(Card newCard) {
-        if (!isServer) {
-            // this was already done for host player
-            handCards.Add(newCard);
+
+        public override void OnNetworkDestroy() {
         }
-    }
 
-    [Server]
-    public void ServerClearCards() {
-        handCards.Clear();
+        public override void OnStartServer() {
 
-        RpcClearCards();
-    }
+        }
 
-    [ClientRpc]
-    private void RpcClearCards() {
-        if (!isServer) {
-            // this was already done for host player
+        public override void OnStartLocalPlayer() {
+
+        }
+
+        [Server]
+        public void ServerAddCard(Card newCard) {
+            if (handCards.Length + 1 < maxHandSize) {
+                handCards.Add(newCard);
+                RpcAddCard(newCard);
+            }
+        }
+        //RPC = Remote Procedure Call. The way to perform an action across a network.
+        //[ClientRpc] is called on the server, but runs on the client.
+        [ClientRpc]
+        void RpcAddCard(Card newCard) {
+            if (!isServer) {
+                // this was already done for host player
+                handCards.Add(newCard);
+            }
+        }
+
+        [Server]
+        public void ServerClearCards() {
             handCards.Clear();
+
+            RpcClearCards();
         }
+
+        [ClientRpc]
+        private void RpcClearCards() {
+            if (!isServer) {
+                // this was already done for host player
+                handCards.Clear();
+            }
+        }
+
+        [Client]
+        public void ShowCards() {
+            var card = handCards[0];
+            card.hidden = false;
+            handCards[0] = card;
+            
+
+        }
+
+        [ClientRpc]
+        public void RpcYourTurn(bool isYourTurn) {
+            // make player who is having current turn green
+            Color c = new Color(1, 1, 1, 0.5f);
+            if (isYourTurn)
+                c = Color.green;
+            
+        }
+
+        //there is also a [Command] tag which means that the command will be sent from the player
+        //object on the client to the player objects on the server. You must also add the 'Cmd' prefix.
+        //Commands should not be sent every frame, this would cause a lot of network traffic.
+
     }
-
-    [Client]
-    public void ShowCards() {
-        var card = handCards[0];
-        card.hidden = false;
-        handCards[0] = card;
-        
-
-    }
-
-    [ClientRpc]
-    public void RpcYourTurn(bool isYourTurn) {
-        // make player who is having current turn green
-        Color c = new Color(1, 1, 1, 0.5f);
-        if (isYourTurn)
-            c = Color.green;
-        
-    }
-
-    //there is also a [Command] tag which means that the command will be sent from the player
-    //object on the client to the player objects on the server. You must also add the 'Cmd' prefix.
-    //Commands should not be sent every frame, this would cause a lot of network traffic.
-
 }
