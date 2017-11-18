4a126c4694d4df69a2bce8c267f5b5e8d51f4797
James Hoak <jah272@case.edu>
2017-11-17 22:45:32 -0600

    Add working NetworkManager with turn management, handlers

--- API/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/NetworkManager.cs
+++ API/UnityAPI/Tableau/Assets/API/Includes/Tableau/Networking/NetworkManager.cs
@@ -1,21 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using Tableau.Base;
+using Tableau.Base.Event;
 
 namespace Tableau.Base.Net {
 
     public class NetworkManager : UnityEngine.Networking.NetworkManager {
 
         const int maxPlayers = 2;
-        static Player[] players = new Player[3];
+        Player[] players = new Player[3];
         private int playerIndex = 0;
 
-        //[SyncVar] ******************Can only use SyncVar in NetworkBehaviour classes.
-        //public int turnPlayerId = getStartingPlayerId();
+        [SyncVar]
+        public int turnPlayerId = getStartingPlayerId();
         // now: don't accept state-changing commands (i.e. moves) from client unless it's their turn
 
-        public static int getStartingPlayerId() {
+        // todo custom attribute for server callback?
+        void Start() {
+            // subscribe to all the general events
+            EventManager em = EventManager.instance;
+            em.StartListening(GeneralEvents.LoadStart, HandleLoadStart);
+            em.StartListening(GeneralEvents.LoadEnd, HandleLoadEnd);
+            em.StartListening(GeneralEvents.GameStart, HandleGameStart);
+            em.StartListening(GeneralEvents.GameEnd, HandleGameEnd);
+            em.StartListening(GeneralEvents.GameRestart, HandleGameRestart);
+            em.StartListening("ChangingTurn", HandleChangingTurn);
+        }
+
+        // todo custom attribute for server callback?
+        void HandleLoadStart() {
+            // do nothing...
+        }
+
+        // todo custom attribute for server callback?
+        void HandleLoadEnd() {
+            // do nothing...
+        }
+
+        // todo custom attribute for server callback?
+        void HandleGameStart() {
+            // do nothing...
+        }
+
+        // todo custom attribute for server callback?
+        void HandleGameEnd() {
+            // do nothing...
+        }
+
+        // todo custom attribute for server callback?
+        void HandleGameRestart() {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        // todo does this need a custom attribute?
+        void HandleChangingTurn() {
+            turnPlayerId = getNextPlayerId();
+        }
+
+        public virtual int getStartingPlayerId() {
             return players[0].playerId;
         }
 
@@ -27,9 +72,9 @@ namespace Tableau.Base.Net {
         public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId) {
 
             for(int i = 1; i < maxPlayers + 1; i++) {
-                if (players[i] == null) {
+                if (numSlots[i] == null) {
                     GameObject playerObj = (GameObject)GameObject.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-                    Player player = playerObj.GetComponent<Player>();
+                    Player player = playerObj.getComponent<Player>();
                     player.playerId = i;
                     players[i] = player;
                     NetworkServer.AddPlayerForConnection(conn, playerObj, playerControllerId);
@@ -40,9 +85,9 @@ namespace Tableau.Base.Net {
         }
 
         public override void OnServerRemovePlayer(NetworkConnection conn, PlayerController playerController) {
-            Player player = playerController.gameObject.GetComponent<Player>();
+            playerController player = playerController.gameObject.GetComponent<Player>();
             players[player.playerId] = null;
-            base.OnServerRemovePlayer(conn, playerController);
+            base.OnserverRemovePlayer(conn, playerController);
         }
 
         public override void OnServerDisconnect(NetworkConnection conn) {
